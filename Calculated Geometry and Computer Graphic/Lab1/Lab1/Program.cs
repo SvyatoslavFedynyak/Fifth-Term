222c7263a856f3d74b71d5747a2562f7d850720c
Svyatoslav Fedynyak <svyatoslav912@gmail.com>
2018-09-22 21:53:43 +0300

    Fixed calculatinad and added user to enter his own data

---
+++ Calculated Geometry and Computer Graphic/Lab1/Lab1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Lab1
+{
+    class Matrix
+    {
+        int numOfRows;
+        int numOfColls;
+        double[,] items;
+        public Matrix(double[,] arr)
+        {
+            items = arr;
+            numOfColls = arr.GetLength(1);
+            numOfRows = arr.GetLength(0);
+        }
+        public Matrix(int numOfRows, int numOfColls)
+        {
+            items = new double[numOfRows, numOfColls];
+            this.numOfRows = numOfRows;
+            this.numOfColls = numOfColls;
+        }
+        public double this[int row, int col]
+        {
+            get { return items[row, col]; }
+            set { items[row, col] = value; }
+        }
+        public static Matrix operator *(Matrix first, Matrix second)
+        {
+            int resultMatrixColls, resultMatrixRows;
+            #region Result Matrix Size Check
+            if (first.numOfColls >= second.numOfColls)
+            {
+                resultMatrixColls = second.numOfColls;
+            }
+            else
+            {
+                resultMatrixColls = first.numOfColls;
+            }
+            if (first.numOfRows >= second.numOfRows)
+            {
+                resultMatrixRows = second.numOfRows;
+            }
+            else
+            {
+                resultMatrixRows = first.numOfRows;
+            }
+            #endregion
+            Matrix result = new Matrix(resultMatrixRows, resultMatrixColls);
+            double resultElement = 0;
+            for (int i = 0; i < resultMatrixRows; i++)
+            {
+                for (int j = 0; j < resultMatrixColls; j++)
+                {
+                    for (int k = 0; k < second.numOfRows; k++)
+                    {
+                        resultElement += first[i, k] * second[k, j];
+                    }
+                    result[i, j] = resultElement;
+                    resultElement = 0;
+                }
+            }
+            return result;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < numOfRows; i++)
+            {
+                for (int j = 0; j < numOfColls; j++)
+                {
+                    sb.AppendFormat($"{items[i, j]} ");
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+    }
+    static class DimetricalProectionBuilder
+    {
+        static DimetricalProectionBuilder()
+        {
+
+        }
+        public static Matrix Build(Matrix transportMatrix, Matrix target)
+        {
+            return target * transportMatrix;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] arr1 = new double[,]
+            {
+                 {4, 6},
+                {7, 8 }
+            };
+            double[,] arr2 = new double[,]
+            {
+                 {3, 8, 2},
+                {5, 2, 8}
+            };
+            Matrix main = new Matrix(arr1);
+            Console.WriteLine(main.ToString());
+            Matrix second = new Matrix(arr2);
+            Console.WriteLine(second.ToString());
+            Matrix res = main * second;
+            Console.WriteLine(res.ToString());
+            Console.ReadLine();
+        }
+    }
+}
--- Numerical Methods/Lab1/Lab1/Program.cs
+++ Numerical Methods/Lab1/Lab1/Program.cs
@@ -16,6 +16,15 @@ namespace Lab1
             this.more = more;
         }
     }
+    struct StartValue
+    {
+        public double x0, y0;
+        public StartValue(double x0, double y0)
+        {
+            this.x0 = x0;
+            this.y0 = y0;
+        }
+    }
     struct SystemAnswer
     {
         public double x, y;
@@ -28,7 +37,7 @@ namespace Lab1
     static class UnlinearEquation
     {
         //3x - cos(x) - 1 = 0
-        static double x0, realResult = 0.6071, a = -1, b = 1;
+        static double x0, realResult = 0.60710164810312263123, a = -1, b = 1;
         public static double f(double value)
         {
             return 3 * value - Cos(value) - 1;
@@ -37,34 +46,41 @@ namespace Lab1
         {
             return 3 + Sin(value);
         }
-        public static double HordMethod(double epsilon)
+        public static double f2(double value)
+        {
+            return Cos(value);
+        }
+        public static double HordMethod(double epsilon, Range functionRange, out int steps)
         {
             //f2 = cos(x)>0 xє[1, 1] тому чи буде f(x0)*f2 додатнім залежить від f(x0)
-            if (f(a) > 0)
+            a = functionRange.less;
+            b = functionRange.more;
+            steps = 0;
+            double x0temp;
+            if (f2(a) * f(a) > 0)
             {
                 x0 = b;
                 do
                 {
-                    if (Abs(x0 - realResult) > epsilon)
-                    {
-                        x0 = a - (x0 - a) * (f(a) / (f(x0) - f(a)));
-                    }
-                    else
+                    x0temp = x0;
+                    x0 = a - (x0 - a) * (f(a) / (f(x0) - f(a)));
+                    steps++;
+                    if (Abs(x0 - x0temp) <= epsilon)
                     {
                         break;
                     }
+
                 } while (true);
             }
-            else if (f(b) > 0)
+            else if (f2(b) * f(b) > 0)
             {
                 x0 = a;
                 do
                 {
-                    if (Abs(x0 - realResult) > epsilon)
-                    {
-                        x0 = x0 - ((b - x0) * (f(x0) / (f(b) - f(x0))));
-                    }
-                    else
+                    x0temp = x0;
+                    x0 = x0 - ((b - x0) * (f(x0) / (f(b) - f(x0))));
+                    steps++;
+                    if (Abs(x0 - x0temp) <= epsilon)
                     {
                         break;
                     }
@@ -76,13 +92,17 @@ namespace Lab1
             }
             return x0;
         }
-        public static double NyutonMethod(double epsilon)
+        public static double NyutonMethod(double epsilon, Range functionRange, out int steps)
         {
-            if (f(a) > 0)
+            steps = 0;
+            a = functionRange.less;
+            b = functionRange.more;
+            double x0temp;
+            if (f2(a) * f(a) > 0)
             {
                 x0 = a;
             }
-            else if (f(b) > 0)
+            else if (f2(b) * f(b) > 0)
             {
                 x0 = b;
             }
@@ -92,47 +112,51 @@ namespace Lab1
             }
             do
             {
-                if (Abs(x0 - realResult) > epsilon)
-                {
-                    x0 = x0 - (f(x0) / f1(x0));
-                }
-                else
+                x0temp = x0;
+                x0 = x0 - (f(x0) / f1(x0));
+                steps++;
+                if (Abs(x0 - x0temp) <= epsilon)
                 {
                     break;
                 }
             } while (true);
             return x0;
         }
-        public static Range CombineMethod(double epsilon)
+        public static Range CombineMethod(double epsilon, Range functionRange, out int steps)
         {
+            steps = 0;
+            a = functionRange.less;
+            b = functionRange.more;
+            double lesstemp, moretemp, temp;
             double less, more;
-            if (f(a) > 0)
+            if (f2(a) * f(a) > 0)
             {
                 less = a; more = b;
                 do
                 {
-                    if (Abs(less - realResult) > epsilon || Abs(more - realResult) > epsilon)
-                    {
-                        more = more - (more - less) * (f(less) / (f(more) - f(less)));
-                        less = less - (f(less) / f1(less));
-                    }
-                    else
+                    moretemp = more;
+                    lesstemp = less;
+                    more = more - (more - less) * (f(less) / (f(more) - f(less)));
+                    less = less - (f(less) / f1(less));
+                    steps++;
+                    if (Abs(more - moretemp) <= epsilon && Abs(less - lesstemp) <= epsilon)
                     {
                         break;
                     }
                 } while (true);
             }
-            else if (f(b) > 0)
+            else if (f2(b) * f(b) > 0)
             {
                 less = a; more = b;
                 do
                 {
-                    if (Abs(less - realResult) > epsilon || Abs(more - realResult) > epsilon)
-                    {
-                        more = more - (f(more) / f1(more));
-                        less = less - (more - less) * (f(less) / (f(more) - f(less)));
-                    }
-                    else
+                    moretemp = more;
+                    lesstemp = less;
+                    temp = more;
+                    more = more - (f(more) / f1(more));
+                    less = less - (temp - less) * (f(less) / (f(temp) - f(less)));
+                    steps++;
+                    if (Abs(more - moretemp) <= epsilon && Abs(less - lesstemp) <= epsilon)
                     {
                         break;
                     }
@@ -153,7 +177,7 @@ namespace Lab1
         //G'x = - 2
         //F'y = -1
         //G'y = cosy 
-        static double x0, y0, a = -2, b = 0, c = -2, d = 0, realX = -0.945011, realY = -1.09739;
+        static double x0 = -1, y0 = -1, realX = -0.945011, realY = -1.09739;
         private static double F(double x, double y)
         {
             return Cos(x + 0.5) - y - 2;
@@ -190,29 +214,29 @@ namespace Lab1
         {
             return F(x, y) * (-2) - G(x, y) * F1x(x);
         }
-        public static SystemAnswer IterationMethod(double epsilon)
+        public static SystemAnswer IterationMethod(double epsilon, StartValue functionStartValue, out int steps)
         {
-            x0 = -1; y0 = -1;
-            double temp;
+            x0 = functionStartValue.x0; y0 = functionStartValue.y0;
+            steps = 0;
+            double tempx0, tempy0;
             do
             {
-                if (Abs(x0 - realX) > epsilon || Abs(y0 - realY) > epsilon)
-                {
-                    temp = x0;
-                    x0 = gx(y0);
-                    y0 = fy(temp);
-                }
-                else
+                tempx0 = x0;
+                tempy0 = y0;
+                x0 = gx(y0);
+                y0 = fy(tempx0);
+                steps++;
+                if (Abs(x0 - tempx0) <= epsilon && Abs(y0 - tempy0) <= epsilon)
                 {
                     break;
                 }
             } while (true);
             return new SystemAnswer(x0, y0);
         }
-        public static SystemAnswer NyutonMethod(double epsilon)
+        public static SystemAnswer NyutonMethod(double epsilon, StartValue functionStartValue, out int steps)
         {
-            x0 = -1;
-            y0 = -1;
+            x0 = functionStartValue.x0; y0 = functionStartValue.y0;
+            steps = 0;
             double tempx, tempy;
             do
             {
@@ -221,7 +245,12 @@ namespace Lab1
                     tempx = x0;
                     tempy = y0;
                     x0 = tempx + (deltaXn(tempx, tempy) / deltaN(tempx, tempy));
-                    y0 = tempy + (deltaYn(tempx, tempy) / deltaN(tempx, tempy)); 
+                    y0 = tempy + (deltaYn(tempx, tempy) / deltaN(tempx, tempy));
+                    steps++;
+                    if (Abs(x0 - tempx) <= epsilon && Abs(y0 - tempy) <= epsilon)
+                    {
+                        break;
+                    }
                 }
                 else
                 {
@@ -236,15 +265,36 @@ namespace Lab1
         static void Main(string[] args)
         {
             double epsilon = 0.0001;
-            Console.WriteLine("Please, enter epsilon (with ',')");
-            epsilon = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"Calculating for unlinear function '3x - cosx - 1 = 0', where real result is x0 = 0.607102 with e = {epsilon}");
-            Console.WriteLine($"For Hord Method: x0 =  {UnlinearEquation.HordMethod(epsilon)}");
-            Console.WriteLine($"For Nyuton Method: x0 =  {UnlinearEquation.NyutonMethod(epsilon)}");
-            Console.WriteLine($"For Combine Method: x01 = {UnlinearEquation.CombineMethod(epsilon).less} and x02 = {UnlinearEquation.CombineMethod(epsilon).more}");
-            Console.WriteLine($"Calculating for system F = cos(x+0.5) - y = 0 and G = siny -2x - 1 = 0, where real result is x0 = -0.945011 y0 = -1.09839 with e = {epsilon}");
-            Console.WriteLine($"For Iteration Method: x0 = {EquationSystem.IterationMethod(epsilon).x} and y0 = {EquationSystem.IterationMethod(epsilon).y}");
-            Console.WriteLine($"For Nuyton Method: x0 = {EquationSystem.NyutonMethod(epsilon).x} and y0 = {EquationSystem.NyutonMethod(epsilon).y}");
+            int steps;
+            Range funcRange = new Range(-1, 1);
+            StartValue funcStartValue = new StartValue(-1, -1);
+            Console.WriteLine("If you want to put your own epsilon enter 'yes', else epsilon will be 0,0001");
+            if (Console.ReadLine() == "yes")
+            {
+                Console.WriteLine("Please, enter epsilon (with ',' and max epsilon = 1E-15)");
+                epsilon = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine("If you want to use your own range for unlinear equation enter 'yes', else range will be [-1, 1]");
+            if (Console.ReadLine() == "yes")
+            {
+                Console.WriteLine("Please, enter your range: first less, than more");
+                funcRange.less = Convert.ToDouble(Console.ReadLine());
+                funcRange.more = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine($"Calculating for unlinear function '3x - cosx - 1 = 0', where real result is x0 = 0.607101648103123 with e = {epsilon}");
+            Console.WriteLine($"For Hord Method: x0 =  {UnlinearEquation.HordMethod(epsilon, funcRange, out steps)} with {steps} steps");
+            Console.WriteLine($"For Nyuton Method: x0 =  {UnlinearEquation.NyutonMethod(epsilon, funcRange, out steps)} with {steps} steps");
+            Console.WriteLine($"For Combine Method: x01 = {UnlinearEquation.CombineMethod(epsilon, funcRange, out steps).less} and x02 = {UnlinearEquation.CombineMethod(epsilon, funcRange, out steps).more} with {steps} steps");
+            Console.WriteLine("\nIf you want to use your own start values enter 'yes', else start values will be -1 and -1");
+            if (Console.ReadLine() == "yes")
+            {
+                Console.WriteLine("Please, enter your start values: first x0, than y0");
+                funcStartValue.x0 = Convert.ToDouble(Console.ReadLine());
+                funcStartValue.y0 = Convert.ToDouble(Console.ReadLine());
+            }
+            Console.WriteLine($"\nCalculating for system F = cos(x+0.5) - y = 0 and G = siny -2x - 1 = 0, where real result is x0 = -0.945011 y0 = -1.09839 with e = {epsilon}");
+            Console.WriteLine($"For Iteration Method: x0 = {EquationSystem.IterationMethod(epsilon, funcStartValue, out steps).x} and y0 = {EquationSystem.IterationMethod(epsilon, funcStartValue, out steps).y} with {steps} steps");
+            Console.WriteLine($"For Nuyton Method: x0 = {EquationSystem.NyutonMethod(epsilon, funcStartValue, out steps).x} and y0 = {EquationSystem.NyutonMethod(epsilon, funcStartValue, out steps).y} with {steps} steps");
             Console.ReadLine();
         }
     }
