3be7e0321f5c466d46c18f2d09c352e54ccbb383
Svyatoslav Fedynyak <svyatoslav912@gmail.com>
2018-10-01 22:28:52 +0300

    Lab 3 ended

--- Distributed and parallel systems/Lab 3/Lab 3/Program.cs
+++ Distributed and parallel systems/Lab 3/Lab 3/Program.cs
@@ -3,14 +3,274 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
 
 namespace Lab_3
 {
-    class Matrix
+    enum CalculatedWay
+    {
+        Up, Down
+    }
+    class Data
+    {
+        public Matrix SLAR;
+        public double[] SLARAnswer;
+        public int firstRow, lastRow;
+        public int numOfBasicRow;
+        public CalculatedWay way;
+        public bool lastCalculation;
+        public Data(Matrix SLAR, double[] SLARAnswer, int firstRow, int lastRow, int numOfBasicRow, CalculatedWay way, bool lastCalculation)
+        {
+            this.SLAR = SLAR;
+            this.SLARAnswer = SLARAnswer;
+            this.firstRow = firstRow;
+            this.lastRow = lastRow;
+            this.numOfBasicRow = numOfBasicRow;
+            this.way = way;
+            this.lastCalculation = lastCalculation;
+        }
+    }
+    public static class GausCalculator
+    {
+        private static AutoResetEvent waitForLastCalculation = new AutoResetEvent(false);
+        public static double[] Calculate(Matrix SLAR, double[] SLARAnswers)
+        {
+            double[] result = new double[SLARAnswers.Length];
+            for (int i = 0; i < SLAR.Rang - 1; i++)
+            {
+                CalculateLines(new Data(SLAR, SLARAnswers, i + 1, SLAR.Rang - 1, i, CalculatedWay.Down, false));
+            }
+            for (int i = SLAR.Rang - 1; i > 0; i--)
+            {
+                CalculateLines(new Data(SLAR, SLARAnswers, 0, i - 1, i, CalculatedWay.Up, false));
+            }
+            for (int i = 0; i < SLARAnswers.Length; i++)
+            {
+                result[i] = SLARAnswers[i] / SLAR[i, i];
+            }
+
+            return result;
+        }
+        public static double[] CalculateAsync(Matrix SLAR, double[] SLARAnswers, int numOfThreads)
+        {
+            double[] result = new double[SLARAnswers.Length];
+            int linesForThreaad, additionalLines, currentBasicLine = 0;
+            int mainTreads = numOfThreads - 1, firstAddtionalLine = 0;
+            int linesToCalculate = SLAR.Rang - 1;
+            #region DownWay
+            do
+            {
+                if (linesToCalculate % mainTreads == 0)
+                {
+                    linesForThreaad = linesToCalculate / mainTreads;
+                    for (int i = 0; i < mainTreads; i++)
+                    {
+                        new Thread(new ParameterizedThreadStart(CalculateLines)).Start(
+                            new Data(SLAR, SLARAnswers, currentBasicLine + linesForThreaad * i + 1, currentBasicLine + linesForThreaad * (i + 1), currentBasicLine, CalculatedWay.Down, i == mainTreads - 1));
+                    }
+                    waitForLastCalculation.WaitOne();
+                }
+                else
+                {
+                    if (linesToCalculate > mainTreads)
+                    {
+                        additionalLines = linesToCalculate % mainTreads;
+                        linesForThreaad = (linesToCalculate - additionalLines) / mainTreads;
+
+                        for (int i = 0; i < mainTreads; i++)
+                        {
+                            new Thread(new ParameterizedThreadStart(CalculateLines)).Start(
+                                new Data(SLAR, SLARAnswers, currentBasicLine + linesForThreaad * i + 1, currentBasicLine + linesForThreaad * (i + 1), currentBasicLine, CalculatedWay.Down, false));
+                            if (i == mainTreads - 1)
+                            {
+                                firstAddtionalLine = currentBasicLine + linesForThreaad * (i + 1) + 1;
+                            }
+                        }
+                        new Thread(new ParameterizedThreadStart(CalculateLines)).Start(
+                            new Data(SLAR, SLARAnswers, firstAddtionalLine, firstAddtionalLine + additionalLines - 1, currentBasicLine, CalculatedWay.Down, true));
+                        waitForLastCalculation.WaitOne();
+                    }
+                    else
+                    {
+                        additionalLines = linesToCalculate;
+                        new Thread(new ParameterizedThreadStart(CalculateLines)).Start(
+                            new Data(SLAR, SLARAnswers, currentBasicLine + 1, currentBasicLine + additionalLines, currentBasicLine, CalculatedWay.Down, true));
+                        waitForLastCalculation.WaitOne();
+                    }
+
+                }
+                linesToCalculate--;
+                if (linesToCalculate == 0)
+                {
+                    break;
+                }
+                currentBasicLine++;
+            } while (true);
+            #endregion
+            #region UpWay
+            linesToCalculate = SLAR.Rang - 1;
+            currentBasicLine = SLAR.Rang - 1;
+            do
+            {
+                if (linesToCalculate % mainTreads == 0)
+                {
+                    linesForThreaad = linesToCalculate / mainTreads;
+                    for (int i = 0; i < mainTreads; i++)
+                    {
+                        new Thread(new ParameterizedThreadStart(CalculateLines)).Start(
+                            new Data(SLAR, SLARAnswers, i * linesForThreaad, (i + 1) * linesForThreaad - 1, currentBasicLine, CalculatedWay.Up, i == mainTreads - 1));
+                    }
+                    waitForLastCalculation.WaitOne();
+                }
+                else
+                {
+                    if (linesToCalculate > mainTreads)
+                    {
+                        additionalLines = linesToCalculate % mainTreads;
+                        linesForThreaad = (linesToCalculate - additionalLines) / mainTreads;
+
+                        for (int i = 0; i < mainTreads; i++)
+                        {
+                            new Thread(new ParameterizedThreadStart(CalculateLines)).Start(
+                                new Data(SLAR, SLARAnswers, i * linesForThreaad, (i + 1) * linesForThreaad - 1, currentBasicLine, CalculatedWay.Up, false));
+                            if (i == mainTreads - 1)
+                            {
+                                firstAddtionalLine = linesForThreaad * (i + 1);
+                            }
+                        }
+                        new Thread(new ParameterizedThreadStart(CalculateLines)).Start(new Data(SLAR, SLARAnswers, firstAddtionalLine, firstAddtionalLine + additionalLines - 1, currentBasicLine, CalculatedWay.Up, true));
+                        waitForLastCalculation.WaitOne();
+                    }
+                    else
+                    {
+                        additionalLines = linesToCalculate;
+                        new Thread(new ParameterizedThreadStart(CalculateLines)).Start(
+                            new Data(SLAR, SLARAnswers, 0, additionalLines - 1, currentBasicLine, CalculatedWay.Up, true));
+                        waitForLastCalculation.WaitOne();
+                    }
+
+                }
+                linesToCalculate--;
+                if (linesToCalculate == 0)
+                {
+                    break;
+                }
+                currentBasicLine--;
+
+            } while (true);
+
+            #endregion
+            for (int i = 0; i < SLARAnswers.Length; i++)
+            {
+                result[i] = SLARAnswers[i] / SLAR[i, i];
+            }
+            return result;
+        }
+        private static void CalculateLines(object data)
+        {
+            Data temp = (Data)data;
+            int roundDeciamls = 2;
+            double coef;
+            if (temp.way == CalculatedWay.Down)
+            {
+                for (int i = temp.firstRow; i <= temp.lastRow; i++)
+                {
+                    coef = -temp.SLAR[temp.numOfBasicRow, temp.numOfBasicRow] / temp.SLAR[i, temp.numOfBasicRow];
+                    for (int j = temp.numOfBasicRow; j < temp.SLAR.Rang; j++)
+                    {
+                        temp.SLAR[i, j] = Math.Round(temp.SLAR[i, j] * coef + temp.SLAR[temp.numOfBasicRow, j], roundDeciamls);
+                    }
+                    temp.SLARAnswer[i] = Math.Round(temp.SLARAnswer[i] * coef + temp.SLARAnswer[temp.numOfBasicRow], roundDeciamls);
+                }
+            }
+            else if (temp.way == CalculatedWay.Up)
+            {
+                for (int i = temp.lastRow; i >= temp.firstRow; i--)
+                {
+                    coef = -temp.SLAR[temp.numOfBasicRow, temp.numOfBasicRow] / temp.SLAR[i, temp.numOfBasicRow];
+                    temp.SLAR[i, temp.numOfBasicRow] = Math.Round(temp.SLAR[i, temp.numOfBasicRow] * coef + temp.SLAR[temp.numOfBasicRow, temp.numOfBasicRow], roundDeciamls);
+                    temp.SLARAnswer[i] = Math.Round(temp.SLARAnswer[i] * coef + temp.SLARAnswer[temp.numOfBasicRow], roundDeciamls);
+                }
+            }
+            if (temp.lastCalculation)
+            {
+                waitForLastCalculation.Set();
+            }
+        }
+    }
+    public static class Functions
+    {
+        static public void FullMatrixWithRandomNumbers(Matrix target, Random randomiser, int min, int max)
+        {
+            for (int i = 0; i < target.Rang; i++)
+            {
+                for (int j = 0; j < target.Rang; j++)
+                {
+                    target[i, j] = randomiser.Next(min, max);
+                }
+            }
+        }
+        static public void FullArrayWithRandomNumbers(double[] target, Random randomiser, int min, int max)
+        {
+            for (int i = 0; i < target.Length; i++)
+            {
+                target[i] = randomiser.Next(min, max);
+            }
+        }
+        static public string ArrToString(double[] target)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (double item in target)
+            {
+                sb.AppendFormat($"{item} ");
+            }
+            return sb.ToString();
+        }
+        static public void GetTime(int sizeOfMatrix, int numOfthreads)
+        {
+            Random randomiser = new Random();
+            Stopwatch timer = new Stopwatch();
+            Matrix test = new Matrix(sizeOfMatrix, sizeOfMatrix);
+            double[] answers = new double[sizeOfMatrix];
+            FullArrayWithRandomNumbers(answers, randomiser, 1, 10);
+            FullMatrixWithRandomNumbers(test, randomiser, 0, 10);
+            timer.Start();
+            GausCalculator.Calculate(test, answers);
+            timer.Stop();
+            Console.WriteLine($"In 1 thread time for SLAR {sizeOfMatrix}x{sizeOfMatrix} is: {timer.Elapsed.ToString()}");
+            timer.Reset();
+            FullArrayWithRandomNumbers(answers, randomiser, 1, 10);
+            FullMatrixWithRandomNumbers(test, randomiser, 0, 10);
+            timer.Start();
+            GausCalculator.CalculateAsync(test, answers, numOfthreads);
+            timer.Stop();
+            Console.WriteLine($"In {numOfthreads} thread time for SLAR {sizeOfMatrix}x{sizeOfMatrix} is: {timer.Elapsed.ToString()}");
+            timer.Reset();
+        }
+
+    }
+
+
+    public class Matrix
     {
         int numOfRows;
         int numOfColls;
         double[,] items;
+        public int Rang
+        {
+            get
+            {
+                if (numOfColls == numOfRows)
+                {
+                    return numOfRows;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
         public Matrix(double[,] arr)
         {
             items = arr;
@@ -25,7 +285,10 @@ namespace Lab_3
         }
         public double this[int row, int col]
         {
-            get { return items[row, col]; }
+            get
+            {
+                return items[row, col];
+            }
             set { items[row, col] = value; }
         }
         public static Matrix operator *(Matrix first, Matrix second)
@@ -83,7 +346,12 @@ namespace Lab_3
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Enter size of matrix:");
+            int size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number of threads");
+            int threads = Convert.ToInt32(Console.ReadLine());
+            Functions.GetTime(size, threads);
+            Console.ReadLine();
         }
     }
 }
---
+++ Distributed and parallel systems/Lab 3/Lab3ModuleTest/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab_3;
+
+namespace Lab3ModuleTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void GausCalculateUnitTest()
+        {
+            Matrix test = new Matrix(new double[,]
+                        {
+                    { 8, 7, 7, 6, 4},
+                    { 4, 5, 6, 9, 9},
+                    { 5, 7, 8, 1, 6},
+                    { 3, 6, 5, 1, 2},
+                    { 6, 2, 3, 7, 6}
+                    });
+            double[] SLARAnswers = new double[] { 4, 2, 8, 5, 4 };
+            double[] expectedResult = new double[]
+            {Math.Round(-0.1276, 1), Math.Round(0.2482, 1) ,Math.Round(0.8204, 1) ,Math.Round(0.2459, 1) ,Math.Round(1.826, 1)};
+            double[] realResult = new double[5];
+            realResult = GausCalculator.Calculate(test, SLARAnswers);
+            for (int i = 0; i < realResult.Length; i++)
+            {
+                realResult[i] = Math.Round(realResult[i], 1);
+            }
+            CollectionAssert.AreEqual(expectedResult, realResult);
+        }
+        [TestMethod]
+        public void GausAsyncCalculateUnitTest()
+        {
+            Matrix test = new Matrix(new double[,]
+                        {
+                    { 8, 7, 7, 6, 4},
+                    { 4, 5, 6, 9, 9},
+                    { 5, 7, 8, 1, 6},
+                    { 3, 6, 5, 1, 2},
+                    { 6, 2, 3, 7, 6}
+                    });
+            double[] SLARAnswers = new double[] { 4, 2, 8, 5, 4 };
+            double[] expectedResult = new double[]
+            {Math.Round(-0.1276, 1), Math.Round(0.2482, 1) ,Math.Round(0.8204, 1) ,Math.Round(0.2459, 1) ,Math.Round(1.826, 1)};
+            double[] realResult = new double[5];
+            realResult = GausCalculator.CalculateAsync(test, SLARAnswers, 4);
+            for (int i = 0; i < realResult.Length; i++)
+            {
+                realResult[i] = Math.Round(realResult[i], 1);
+            }
+            CollectionAssert.AreEqual(expectedResult, realResult);
+        }
+    }
+}
